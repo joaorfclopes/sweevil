9d28fe641cc5bc7c2a5dcbab886059e8c38d473b
Joao Lopes <joaorfclopes@gmail.com>
2020-11-24 20:25:10 +0000

    React Router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Switch>
+            <Route path="/" component={HomeScreen} exact />
+          </Switch>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useMemo } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import $ from "jquery";
 import logo from "../assets/images/brand/logo_white_yellow.PNG";
 
 export default function Navbar() {
-  const options = useMemo(
-    () => ["home", "about", "gallery", "contacts", "shop"],
-    []
-  );
+  const options = ["home", "about", "gallery", "contacts", "shop"];
 
   const openMenu = () => {
     $(".nav-mobile li").first().addClass("hide");
@@ -20,26 +18,16 @@ export default function Navbar() {
     $(".nav-mobile").removeClass("full-screen");
   };
 
-  useEffect(() => {
-    options.forEach((option) => {
-      if (window.location.href.indexOf(`/${option}`) > -1) {
-        $(`.${option}`).addClass("active");
-      }
-    });
-  }, [options]);
-
   return (
     <header className="navbar">
       <div>
-        <a href="./index.html">
+        <Link to="/">
           <img className="logo" src={logo} alt="logo" />
-        </a>
+        </Link>
         <ul className="nav-desktop">
           {options.map((option) => (
             <li key={option}>
-              <a className={option} href={option}>
-                {option}
-              </a>
+              <Link to={option === "home" ? "" : option}>{option}</Link>
             </li>
           ))}
         </ul>
@@ -49,10 +37,8 @@ export default function Navbar() {
             x
           </li>
           {options.map((option) => (
-            <li key={option}>
-              <a className={option} href={option}>
-                {option}
-              </a>
+            <li onClick={closeMenu} key={option}>
+              <Link to={option === "home" ? "" : option}>{option}</Link>
             </li>
           ))}
         </ul>
