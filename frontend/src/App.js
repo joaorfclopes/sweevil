b3963cd4e7491869a11448e52cdc781b677e7247
Joao Lopes <joaorfclopes@gmail.com>
2020-11-26 13:04:23 +0000

    Loading added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import $ from "jquery";
+import Loading from "./screens/LoadingScreen";
 import Navbar from "./components/Navbar";
 import MenuMobile from "./components/MenuMobile";
 import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+      $("html").addClass("fade-in");
+    }, 1500);
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="App">
-        <div className="grid-container">
-          <Navbar />
-          <MenuMobile />
-          <main>
-            <Switch>
-              <Route exact path="/" component={HomeScreen} />
-            </Switch>
-          </main>
-        </div>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="grid-container">
+            <Navbar />
+            <MenuMobile />
+            <main>
+              <Switch>
+                <Route exact path="/" component={HomeScreen} />
+              </Switch>
+            </main>
+          </div>
+        )}
       </div>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { ReactComponent as SpinnerLogo } from "../assets/images/svg/spinner.svg";
+
+export default function Spinner() {
+  return <SpinnerLogo className="spinner" />;
+}
