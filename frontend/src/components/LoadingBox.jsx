52220336acc84d5a97c1b13289cba097106a4420
Joao Lopes <joaorfclopes@gmail.com>
2020-11-28 20:40:20 +0000

    Shop Scren css changes

--- frontend/src/components/LoadingBox.jsx
+++ frontend/src/components/LoadingBox.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import spinner from "../assets/images/brand/spinner.png";
 
-export default function LoadingBox() {
+export default function LoadingBox(props) {
   return (
-    <div className="loading-box">
-      <img className="spinner" src={spinner} alt="spinner" width="50px" />
+    <div className="loading-box" style={{ lineHeight: props.lineHeight }}>
+      <img
+        className="spinner"
+        src={spinner}
+        alt="spinner"
+        width={props.width ? props.width : "50px"}
+      />
     </div>
   );
 }
