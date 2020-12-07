7ae81d2a672f52d3c56afe9d89035d26e323896d
Joao Lopes <joaorfclopes@gmail.com>
2020-12-07 15:04:42 +0000

    Images preview added to product screen

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import $ from "jquery";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { detailsProduct } from "../actions/productActions";
@@ -16,43 +15,90 @@ export default function ProductScreen(props) {
     dispatch(detailsProduct(productId));
   }, [dispatch, productId]);
 
-  const openLarge = (image) => {
-    $(".large").attr("src", image);
-  };
-
   return (
-    <section style={{ marginTop: "5vw" }}>
+    <section className="product-screen" style={{ marginTop: "6vw" }}>
       {loading ? (
         <LoadingBox lineHeight="70vh" width="100px" />
       ) : error ? (
         <MessageBox variant="error">{error}</MessageBox>
       ) : (
-        <div className="product-screen row center">
-          <div className="product-images">
-            <LazyImage
-              className="large"
-              src={product.images[0]}
-              alt={product.name}
-            />
-            {product.images &&
-              product.images.map((image) => (
-                <span key={image} onClick={() => openLarge(image)}>
-                  <LazyImage className="small" src={image} alt={product.name} />
-                </span>
-              ))}
-          </div>
-          <div className="product-details">
-            <ul>
-              <li>{product.name}</li>
-              <li>{product.category}</li>
-              <li>
+        <div className="container">
+          <div className="row-bootstrap">
+            <div className="col-md-2"></div>
+            <div className="col-md-5">
+              <div
+                id="productImageCarousel"
+                className="carousel slide"
+                data-interval="false"
+              >
+                <div className="carousel-inner">
+                  {product.images.map((image) => (
+                    <div
+                      key={image}
+                      className={`carousel-item ${
+                        image === product.images[0] && "active"
+                      }`}
+                    >
+                      <LazyImage
+                        src={image}
+                        className="d-block w-100"
+                        alt="product"
+                      />
+                    </div>
+                  ))}
+                </div>
+                <a
+                  className="carousel-control-prev"
+                  href="#productImageCarousel"
+                  role="button"
+                  data-slide="prev"
+                >
+                  <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                </a>
+                <a
+                  className="carousel-control-next"
+                  href="#productImageCarousel"
+                  role="button"
+                  data-slide="next"
+                >
+                  <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                </a>
+              </div>
+              <div className="image-preview-container">
+                {product.images.map((image) => (
+                  <div key={image} className="image-preview">
+                    <LazyImage
+                      src={image}
+                      className="d-block w-100"
+                      alt="product"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="col-md-5">
+              <h2>
+                <b>{product.name}</b>
+              </h2>
+              <p className="price">
                 {product.finalPrice
                   ? product.finalPrice.toFixed(2)
                   : product.finalPrice}
                 €
-              </li>
-              <li>{product.description}</li>
-            </ul>
+              </p>
+              <p>
+                <b>Availability:</b> <span className="stock"></span>
+              </p>
+              <p>
+                <b>Category:</b> {product.category}
+              </p>
+            </div>
           </div>
         </div>
       )}
