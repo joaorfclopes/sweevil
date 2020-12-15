81472010b06731921dc8c87173171ec041a9f9d7
Joao Lopes <joaorfclopes@gmail.com>
2020-12-15 23:58:27 +0000

    Admin Screen added

--- frontend/src/screens/AdminScreen.jsx
+++ frontend/src/screens/AdminScreen.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import ProductsTable from "../components/ProductsTable";
+import { signout } from "../actions/userActions";
+import { emptyCart } from "../actions/cartActions";
 
 export default function AdminScreen() {
+  const dispatch = useDispatch();
+
+  const signoutHandler = () => {
+    dispatch(signout());
+    dispatch(emptyCart());
+  };
+
   return (
-    <section>
-      <h1>Admin</h1>
+    <section className="admin-screen">
+      <Link to="/">
+        <button className="primary" onClick={signoutHandler}>
+          Log Out
+        </button>
+      </Link>
+      <ProductsTable />
     </section>
   );
 }
