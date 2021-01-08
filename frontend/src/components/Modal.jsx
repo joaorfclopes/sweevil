88e9270f14ae56dff0417abecb61a12de962ccd4
Joao Lopes <joaorfclopes@gmail.com>
2021-01-08 20:24:51 +0000

    Modal done

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -20,7 +20,52 @@ export default function Modal(props) {
           }}
         >
           <Fade in={props.open}>
-            <LazyImage src={props.img} alt="img" />
+            <div className="modal-images">
+              <div
+                id="modalImageCarousel"
+                className="carousel"
+                data-interval="false"
+              >
+                <div className="carousel-inner">
+                  {props.images.map((image) => (
+                    <div
+                      key={image}
+                      className={`carousel-item ${
+                        image === props.images[props.imgIndex] && "active"
+                      }`}
+                    >
+                      <LazyImage
+                        src={image}
+                        className="d-block w-100"
+                        alt="img"
+                      />
+                    </div>
+                  ))}
+                </div>
+                <a
+                  className="carousel-control-prev"
+                  href="#modalImageCarousel"
+                  role="button"
+                  data-slide="prev"
+                >
+                  <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                </a>
+                <a
+                  className="carousel-control-next"
+                  href="#modalImageCarousel"
+                  role="button"
+                  data-slide="next"
+                >
+                  <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                </a>
+              </div>
+            </div>
           </Fade>
         </Dialog>
       )}
