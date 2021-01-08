1fc48ee75dcec4e63908853d8e62a1a320b7cd21
Joao Lopes <joaorfclopes@gmail.com>
2021-01-08 19:47:04 +0000

    Modal added to product screen

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Dialog, Backdrop, Fade } from "@material-ui/core";
+import LazyImage from "./LazyImage";
+import { isMobile } from "../utils";
 
-export default function Modal() {
+export default function Modal(props) {
   return (
-    <div className="modal">
-      <div></div>
-    </div>
+    <>
+      {props.noMobile && !isMobile() && (
+        <Dialog
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className="modal"
+          open={props.open}
+          onClose={props.handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={props.open}>
+            <LazyImage src={props.img} alt="img" />
+          </Fade>
+        </Dialog>
+      )}
+    </>
   );
 }
