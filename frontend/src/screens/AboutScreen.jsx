24dc29189e025c4fc61ecaba29d662c434d0ddda
Joao Lopes <joaorfclopes@gmail.com>
2021-01-10 21:02:27 +0000

    Video added to about screen

--- frontend/src/screens/AboutScreen.jsx
+++ frontend/src/screens/AboutScreen.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Player } from "video-react";
+import video from "../assets/images/video/video.mov";
+import poster from "../assets/images/video/poster.png";
 
 export default function AboutScreen() {
-  return <section className="about"></section>;
+  return (
+    <section className="about">
+      <div className="video">
+        <Player playsInline poster={poster} src={video} />
+      </div>
+    </section>
+  );
 }
