bd7d863bca24ce7fc558bbb92cc661b8095292e4
Joao Lopes <joaorfclopes@gmail.com>
2021-01-11 13:23:44 +0000

    Video format changed

--- frontend/src/screens/AboutScreen.jsx
+++ frontend/src/screens/AboutScreen.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Player } from "video-react";
-import video from "../assets/images/video/video.mov";
+import video from "../assets/images/video/video.mp4";
 import poster from "../assets/images/video/poster.png";
 
 export default function AboutScreen() {
   return (
     <section className="about">
       <div className="video">
-        <Player playsInline poster={poster} src={video} />
+        <video id="video" poster={poster} controls>
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </section>
   );
