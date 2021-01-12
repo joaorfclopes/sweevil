3eddf70ef48a4ff639684e496190091a2f659f4e
Joao Lopes <joaorfclopes@gmail.com>
2021-01-12 13:38:55 +0000

    About page content added

--- frontend/src/screens/AboutScreen.jsx
+++ frontend/src/screens/AboutScreen.jsx
@@ -15,21 +15,63 @@ export default function AboutScreen() {
     videoElem.onpause = () => {
       playElem.classList.remove("hide");
     };
+    videoElem.onended = () => {
+      videoElem.pause();
+      videoElem.removeAttribute("src");
+      videoElem.load();
+    };
   }, []);
 
   return (
     <section className="about">
-      <div
-        className="video"
-        onMouseEnter={() => setPlay(true)}
-        onMouseLeave={() => setPlay(false)}
-      >
-        <video id="video" poster={poster} controls={play}>
-          <source src={video} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <div className="play">
-          <Play />
+      <div className="row" style={{ minHeight: "70vh" }}>
+        <div className="container">
+          <div className="row-bootstrap">
+            <div className="col-md-5">
+              <p style={{ marginTop: 0 }}>
+                Sílvia Peralta is a multi-disciplinary artist from Portugal born
+                in 1992.
+              </p>
+              <p>
+                In 2013 after finishing a BfA in Design studies in Porto she
+                started tattooing independently.
+              </p>
+              <p>
+                As a tattoo artist she specialized in blackwork developing her
+                own designs in a wide range of styles. Peralta has guested in
+                severall tattooshops including “Covil Tattoo” São Paulo
+                (Brasil), “Eclipse Tattoo” (Barcelona), “The Bonfire Tattoo”
+                Madrid (Spain), “Chill Or Die” Paris (France), as well as others
+                in Germany, Switzerland and Portugal.
+              </p>
+              <p>
+                Currently she’s working as graphic designer, tattoo artist and
+                painter.
+              </p>
+              <p>
+                Sweevil signature comes up from bounding sweet and evil,
+                representing her work mainly based on the concept of duality and
+                opposites, their equilibrium and interpretation of freedom. Her
+                work is an invitation to embrace life observing both parts of
+                every coin free from judgments.
+              </p>
+            </div>
+            <div className="col-md-5">
+              <div
+                className="video"
+                onMouseEnter={() => setPlay(true)}
+                onMouseLeave={() => setPlay(false)}
+              >
+                <video id="video" poster={poster} controls={play}>
+                  <source src={video} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+                <div className="play">
+                  <Play />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
