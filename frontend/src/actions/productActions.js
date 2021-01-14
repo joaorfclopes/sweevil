48fa618ab4ff05a0fd7d19a8148b1c595b44b4dc
Joao Lopes <joaorfclopes@gmail.com>
2021-01-14 23:08:44 +0000

    Loading removed from shop and product

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,10 +1,8 @@
 import Axios from "axios";
 import {
   PRODUCT_DETAILS_FAIL,
-  PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
-  PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_CREATE_REQUEST,
   PRODUCT_CREATE_SUCCESS,
@@ -18,7 +16,6 @@ import {
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
-  dispatch({ type: PRODUCT_LIST_REQUEST });
   try {
     const { data } = await Axios.get("/api/products");
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
@@ -28,7 +25,6 @@ export const listProducts = () => async (dispatch) => {
 };
 
 export const detailsProduct = (productId) => async (dispatch) => {
-  dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
   try {
     const { data } = await Axios.get(`/api/products/${productId}`);
     dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
