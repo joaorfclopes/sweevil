fad3dbb521f7e52de5515a9b0794625ce0edfc92
Joao Lopes <joaorfclopes@gmail.com>
2021-01-16 17:11:35 +0000

    Seed routes updated (test)

--- backend/routes/galleryImageRoute.js
+++ backend/routes/galleryImageRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import data from "../data.js";
 import GalleryImage from "../models/galleryImageModel.js";
+import { isAuth, isAdmin } from "../utils.js";
 
 const galleryImageRouter = express.Router();
 
@@ -15,6 +16,8 @@ galleryImageRouter.get(
 
 galleryImageRouter.get(
   "/seed",
+  isAuth,
+  isAdmin,
   expressAsyncHandler(async (req, res) => {
     const createdGallery = await GalleryImage.insertMany(data.galleryImages);
     res.send({ createdGallery });
