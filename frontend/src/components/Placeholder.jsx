f158d26e14bbe7a9014c2bff09bba323978209fd
Joao Lopes <joaorfclopes@gmail.com>
2021-01-18 20:22:15 +0000

    Placeholder added to products

--- frontend/src/components/Placeholder.jsx
+++ frontend/src/components/Placeholder.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function PlaceHolder({ children, height }) {
+export default function PlaceHolder({ children, height, hide }) {
   return (
-    <div className="placeholder" style={{ height: height }}>
+    <div className={`placeholder ${hide && "hide"}`} style={{ height: height }}>
       {children}
     </div>
   );
