de76d0e60c33368d563f6c2f101a6a95c37e62a1
Joao Lopes <joaorfclopes@gmail.com>
2021-01-19 20:55:18 +0000

    About page fixed and breakpoint added

--- frontend/src/screens/AboutScreen.jsx
+++ frontend/src/screens/AboutScreen.jsx
@@ -40,68 +40,62 @@ export default function AboutScreen(props) {
       variants={props.pageVariants}
       transition={props.pageTransition}
     >
-      <div className="row" style={{ minHeight: "70vh" }}>
-        <div className="container">
-          <div className="row-bootstrap">
-            <div className="col-md-5">
-              <h1 className="title">Who's Sweevil?</h1>
-              <p>
-                Sílvia Peralta is a multi-disciplinary artist from Portugal born
-                in 1992.
-              </p>
+      <div className="row center" style={{ minHeight: "70vh" }}>
+        <div className="text">
+          <h1 className="title">Who's Sweevil?</h1>
+          <p>
+            Sílvia Peralta is a multi-disciplinary artist from Portugal born in
+            1992.
+          </p>
 
-              <p>
-                In 2013 after finishing a BfA in Design studies in Porto she
-                started tattooing independently.
-              </p>
-              <p>
-                As a tattoo artist she specialized in blackwork developing her
-                own designs in a wide range of styles. Peralta has guested in
-                severall tattooshops including “Covil Tattoo” São Paulo
-                (Brasil), “Eclipse Tattoo” (Barcelona), “The Bonfire Tattoo”
-                Madrid (Spain), “Chill Or Die” Paris (France), as well as others
-                in Germany, Switzerland and Portugal.
-              </p>
-              <p>
-                Currently she’s working as graphic designer, tattoo artist and
-                painter.
-              </p>
-              <p>
-                Sweevil signature comes up from bounding sweet and evil,
-                representing her work mainly based on the concept of duality and
-                opposites, their equilibrium and interpretation of freedom. Her
-                work is an invitation to embrace life observing both parts of
-                every coin free from judgments.
-              </p>
+          <p>
+            In 2013 after finishing a BfA in Design studies in Porto she started
+            tattooing independently.
+          </p>
+          <p>
+            As a tattoo artist she specialized in blackwork developing her own
+            designs in a wide range of styles. Peralta has guested in severall
+            tattooshops including “Covil Tattoo” São Paulo (Brasil), “Eclipse
+            Tattoo” (Barcelona), “The Bonfire Tattoo” Madrid (Spain), “Chill Or
+            Die” Paris (France), as well as others in Germany, Switzerland and
+            Portugal.
+          </p>
+          <p>
+            Currently she’s working as graphic designer, tattoo artist and
+            painter.
+          </p>
+          <p>
+            Sweevil signature comes up from bounding sweet and evil,
+            representing her work mainly based on the concept of duality and
+            opposites, their equilibrium and interpretation of freedom. Her work
+            is an invitation to embrace life observing both parts of every coin
+            free from judgments.
+          </p>
+        </div>
+        <div
+          className="video"
+          onMouseEnter={() => setPlay(true)}
+          onMouseLeave={() => setPlay(false)}
+        >
+          <Placeholder height="100%">
+            <video
+              id="video"
+              poster={window.location.origin + "/sweevil.jpg"}
+              controls={play}
+            >
+              <source src={video} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            <div className="play">
+              <Play />
             </div>
-            <div className="col-md-5">
-              <div
-                className="video"
-                onMouseEnter={() => setPlay(true)}
-                onMouseLeave={() => setPlay(false)}
-              >
-                <Placeholder height="100%">
-                  <video
-                    id="video"
-                    poster={window.location.origin + "/sweevil.jpg"}
-                    controls={play}
-                  >
-                    <source src={video} type="video/mp4" />
-                    Your browser does not support the video tag.
-                  </video>
-                  <div className="play">
-                    <Play />
-                  </div>
-                  <div className="poster">
-                    <LazyLoadImage
-                      src={window.location.origin + "/sweevil.jpg"}
-                      afterLoad={videoLoaded}
-                    />
-                  </div>
-                </Placeholder>
-              </div>
+            <div className="poster">
+              <LazyLoadImage
+                src={window.location.origin + "/sweevil.jpg"}
+                afterLoad={videoLoaded}
+              />
             </div>
-          </div>
+          </Placeholder>
         </div>
       </div>
     </motion.section>
