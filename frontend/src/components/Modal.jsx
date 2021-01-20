44c57c9f3f0dbccae6030bb826a608a7af48da64
Joao Lopes <joaorfclopes@gmail.com>
2021-01-20 23:14:51 +0000

    Modal re-added to product screen

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,69 +1,65 @@
 import React from "react";
 import { Dialog, Backdrop, Fade } from "@material-ui/core";
-import { isMobile } from "../utils";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function Modal(props) {
   return (
-    <>
-      {props.noMobile && !isMobile() && (
-        <Dialog
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
-          className="modal"
-          open={props.open}
-          onClose={props.handleClose}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          BackdropProps={{
-            timeout: 500,
-          }}
-        >
-          <Fade in={props.open}>
-            <div className="modal-images">
-              <div
-                id="modalImageCarousel"
-                className="carousel"
-                data-interval="false"
-              >
-                <div className="carousel-inner">
-                  {props.images.map((image) => (
-                    <div
-                      key={image}
-                      className={`carousel-item ${
-                        image === props.images[props.imgIndex] && "active"
-                      }`}
-                    >
-                      <img src={image} className="d-block w-100" alt="img" />
-                    </div>
-                  ))}
+    <Dialog
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      className="modal"
+      open={props.open}
+      onClose={props.handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={props.open}>
+        <div className="modal-images">
+          <div
+            id="modalImageCarousel"
+            className="carousel"
+            data-interval="false"
+          >
+            <div className="carousel-inner">
+              {props.images.map((image) => (
+                <div
+                  key={image}
+                  className={`carousel-item ${
+                    image === props.images[props.imgIndex] && "active"
+                  }`}
+                >
+                  <LazyLoadImage src={image} alt="img" />
                 </div>
-                <a
-                  className="carousel-control-prev"
-                  href="#modalImageCarousel"
-                  role="button"
-                  data-slide="prev"
-                >
-                  <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  ></span>
-                </a>
-                <a
-                  className="carousel-control-next"
-                  href="#modalImageCarousel"
-                  role="button"
-                  data-slide="next"
-                >
-                  <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                  ></span>
-                </a>
-              </div>
+              ))}
             </div>
-          </Fade>
-        </Dialog>
-      )}
-    </>
+            <a
+              className="carousel-control-prev"
+              href="#modalImageCarousel"
+              role="button"
+              data-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+            </a>
+            <a
+              className="carousel-control-next"
+              href="#modalImageCarousel"
+              role="button"
+              data-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+            </a>
+          </div>
+        </div>
+      </Fade>
+    </Dialog>
   );
 }
