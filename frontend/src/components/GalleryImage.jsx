05c95c10b9acd3aea59f1f01ee94d113364841a7
Joao Lopes <joaorfclopes@gmail.com>
2021-01-25 19:11:18 +0000

    Lightbox fixed

--- frontend/src/components/GalleryImage.jsx
+++ frontend/src/components/GalleryImage.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import $ from "jquery";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import Placeholder from "./Placeholder";
+import GalleryModal from "./GalleryModal";
 
 export default function GalleryImage(props) {
   const { galleryImage } = props;
 
+  const [openModal, setOpenModal] = useState(false);
+
   const imageLoaded = (id) => {
     $(`#${id}-gallery-img`).addClass("show");
+  };
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
   };
 
   return (
@@ -16,6 +27,7 @@ export default function GalleryImage(props) {
         <div
           id={`${galleryImage._id}-gallery-img`}
           className="gallery-image-inner"
+          onClick={handleOpenModal}
         >
           <LazyLoadImage
             src={galleryImage.image}
@@ -24,6 +36,11 @@ export default function GalleryImage(props) {
           />
         </div>
       </Placeholder>
+      <GalleryModal
+        open={openModal}
+        handleClose={handleCloseModal}
+        image={galleryImage.image}
+      />
     </div>
   );
 }
--- frontend/src/components/GalleryModal.jsx
+++ frontend/src/components/GalleryModal.jsx
@@ -1,24 +1,7 @@
 import React from "react";
 import { Dialog, Backdrop, Fade } from "@material-ui/core";
-import { useSwipeable } from "react-swipeable";
-import Placeholder from "./Placeholder";
 
 export default function GalleryModal(props) {
-  const next = () => {
-    document.getElementById("carousel-control-next").click();
-  };
-
-  const previous = () => {
-    document.getElementById("carousel-control-prev").click();
-  };
-
-  const swipeHandlers = useSwipeable({
-    onSwipedLeft: () => next(),
-    onSwipedRight: () => previous(),
-    preventDefaultTouchmoveEvent: true,
-    trackMouse: true,
-  });
-
   return (
     <Dialog
       aria-labelledby="transition-modal-title"
@@ -33,53 +16,10 @@ export default function GalleryModal(props) {
       }}
     >
       <Fade in={props.open}>
-        <div className="modal-images">
-          <div
-            id="modalImageCarousel"
-            className="carousel slide"
-            data-interval="false"
-          >
-            <div {...swipeHandlers} className="carousel-inner">
-              {props.images.map((image, index) => (
-                <div
-                  key={index}
-                  className={`carousel-item modal-image ${
-                    image === props.images[props.imgIndex] && "active"
-                  }`}
-                >
-                  <Placeholder>
-                    <div
-                      id={`${index}-modal-image`}
-                      className="modal-image-inner"
-                    >
-                      <img src={image} alt="img" />
-                    </div>
-                  </Placeholder>
-                </div>
-              ))}
-            </div>
-          </div>
+        <div className="modal-image">
+          <img src={props.image} alt="img" />
         </div>
       </Fade>
-      <div className="nav-buttons custom-font">
-        <a
-          id="carousel-control-prev"
-          href="#modalImageCarousel"
-          role="button"
-          data-slide="prev"
-        >
-          Previous
-        </a>{" "}
-        /{" "}
-        <a
-          id="carousel-control-next"
-          href="#modalImageCarousel"
-          role="button"
-          data-slide="next"
-        >
-          Next
-        </a>
-      </div>
     </Dialog>
   );
 }
