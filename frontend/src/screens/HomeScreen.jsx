2e622876a1ce471c7d32cc6b80b6806b9689e4f4
Joao Lopes <joaorfclopes@gmail.com>
2021-01-26 19:36:24 +0000

    Svg to home page added

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { ReactComponent as Doll1 } from "../assets/svg/home/doll1.svg";
+import { ReactComponent as Doll2 } from "../assets/svg/home/doll2.svg";
+import { ReactComponent as Doll3 } from "../assets/svg/home/doll3.svg";
+import { ReactComponent as Doll4 } from "../assets/svg/home/doll4.svg";
 
 export default function HomeScreen(props) {
   return (
     <motion.section
-      className="not-found-screen"
+      className="home"
       initial="out"
       animate="in"
       exit="out"
       variants={props.pageVariants}
       transition={props.pageTransition}
-    ></motion.section>
+    >
+      <div className="row center home-container">
+        <div className="illustration">
+          <Doll1 />
+          <Doll2 />
+          <Doll3 />
+          <Doll4 />
+        </div>
+      </div>
+    </motion.section>
   );
 }
