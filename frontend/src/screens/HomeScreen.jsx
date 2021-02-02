653119139cf6d85b5b65e00965625e1a9dc3bfbc
Joao Lopes <joaorfclopes@gmail.com>
2021-02-02 18:35:52 +0000

    Home illustrations changed for mobile

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import $ from "jquery";
 import { motion } from "framer-motion";
 import Slider from "react-slick";
 import { ReactComponent as Home1 } from "../assets/svg/home/home1.svg";
 import { ReactComponent as Home2 } from "../assets/svg/home/home2.svg";
 import { ReactComponent as Home3 } from "../assets/svg/home/home3.svg";
 import { ReactComponent as Home4 } from "../assets/svg/home/home4.svg";
-import { ReactComponent as Doll1 } from "../assets/svg/home/doll1.svg";
-import { ReactComponent as Doll2 } from "../assets/svg/home/doll2.svg";
-import { ReactComponent as Doll3 } from "../assets/svg/home/doll3.svg";
-import { ReactComponent as Doll4 } from "../assets/svg/home/doll4.svg";
+import { ReactComponent as Emotion1 } from "../assets/svg/home/emotion1.svg";
+import { ReactComponent as Emotion2 } from "../assets/svg/home/emotion2.svg";
+import { ReactComponent as Emotion3 } from "../assets/svg/home/emotion3.svg";
+import { ReactComponent as Emotion4 } from "../assets/svg/home/emotion4.svg";
 import Illustration from "../components/Illustration";
 
 export default function HomeScreen(props) {
@@ -24,6 +25,12 @@ export default function HomeScreen(props) {
     swipe: false,
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      $(".home-slider").addClass("show");
+    }, 50);
+  }, []);
+
   return (
     <motion.section
       className="home"
@@ -34,24 +41,24 @@ export default function HomeScreen(props) {
       transition={props.pageTransition}
     >
       <div className="row center home-container">
-        <div className="home-slider">
+        <div className="home-slider hidden">
           <Slider {...settings}>
             <Illustration
               active
               illustrationLg={<Home1 />}
-              illustrationSm={<Doll1 />}
+              illustrationSm={<Emotion1 />}
             />
             <Illustration
               illustrationLg={<Home2 />}
-              illustrationSm={<Doll2 />}
+              illustrationSm={<Emotion2 />}
             />
             <Illustration
               illustrationLg={<Home3 />}
-              illustrationSm={<Doll3 />}
+              illustrationSm={<Emotion3 />}
             />
             <Illustration
               illustrationLg={<Home4 />}
-              illustrationSm={<Doll4 />}
+              illustrationSm={<Emotion4 />}
             />
           </Slider>
         </div>
