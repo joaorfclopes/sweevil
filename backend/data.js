7f3eefa1bafe9e10d33960ab1820bc79af093f04
Joao Lopes <joaorfclopes@gmail.com>
2021-02-05 05:16:42 +0000

    Data file changed

--- backend/data.js
+++ backend/data.js
@@ -121,146 +121,124 @@ const data = {
   galleryImages: [
     {
       description: "“Against the law’, 2020. Acrylic on paper",
-      image:
-        "/images/painting6.jpg",
+      image: "/images/painting6.jpg",
       category: "Paintings",
     },
     {
       description: "“Isolation”, 2020. Acrylic on paper",
-      image:
-        "/images/painting2.jpg",
+      image: "/images/painting2.jpg",
       category: "Paintings",
     },
     {
       description: "Portugal, 2020",
-      image:
-        "/images/tattoo4.jpg",
+      image: "/images/tattoo4.jpg",
       category: "Tattoos",
     },
     {
       description: "Portugal, 2020",
-      image:
-        "/images/tattoo5.jpg",
+      image: "/images/tattoo5.jpg",
       category: "Tattoos",
     },
     {
-      description: "Portugal, 2020",
-      image:
-        "/images/painting3.jpg",
+      description: "“The system”, 2020. Pencil ",
+      image: "/images/painting3.jpg",
       category: "Paintings",
     },
     {
       description: "Portugal, 2020",
-      image:
-        "/images/tattoo6.jpg",
+      image: "/images/tattoo6.jpg",
       category: "Tattoos",
     },
     {
       description: "“Strange box”, 2020. Acrylic on paper",
-      image:
-        "/images/painting4.jpg",
+      image: "/images/painting4.jpg",
       category: "Paintings",
     },
     {
       description: "Portugal, 2020",
-      image:
-        "/images/tattoo7.jpg",
+      image: "/images/tattoo7.jpg",
       category: "Tattoos",
     },
     {
-      description: "“It didn’t came from outer space”, 2020. Digital illustration",
-      image:
-        "/images/digital2.jpg",
+      description:
+        "“It didn’t came from outer space”, 2020. Digital illustration",
+      image: "/images/digital2.jpg",
       category: "Digital",
     },
     {
       description: "“Reality is equilibrium”, 2020. Acrylic on paper",
-      image:
-        "/images/painting1.jpg",
+      image: "/images/painting1.jpg",
       category: "Paintings",
     },
     {
-      description: "Detail from “Reality is equilibrium”, 2020. Acrylic on paper",
-      image:
-        "/images/painting8.jpg",
+      description:
+        "Detail from “Reality is equilibrium”, 2020. Acrylic on paper",
+      image: "/images/painting8.jpg",
       category: "Paintings",
     },
     {
       description: "Portugal, 2020",
-      image:
-        "/images/tattoo9.jpg",
+      image: "/images/tattoo9.jpg",
       category: "Tattoos",
     },
     {
       description: "“We’re players”, 2020. Digital illustration",
-      image:
-        "/images/digital4.jpg",
+      image: "/images/digital4.jpg",
       category: "Digital",
     },
     {
       description: "“Solid gold”, 2020. Acrylic on paper",
-      image:
-        "/images/painting9.jpg",
+      image: "/images/painting9.jpg",
       category: "Paintings",
     },
     {
       description: "“Equilibrium is everywhere”, 2020. Digital illustration",
-      image:
-        "/images/digital1.jpg",
+      image: "/images/digital1.jpg",
       category: "Digital",
     },
     {
       description: "“Illusion”, 2020. Acrylic on paper",
-      image:
-        "/images/painting10.jpg",
+      image: "/images/painting10.jpg",
       category: "Paintings",
     },
     {
       description: "Portugal, 2020",
-      image:
-        "/images/tattoo1.jpg",
+      image: "/images/tattoo1.jpg",
       category: "Tattoos",
     },
     {
       description: "“The new original sin”, 2020. Acrylic on canvas",
-      image:
-        "/images/painting11.jpg",
+      image: "/images/painting11.jpg",
       category: "Paintings",
     },
     {
       description: "Portugal, 2020",
-      image:
-        "/images/tattoo2.jpg",
+      image: "/images/tattoo2.jpg",
       category: "Tattoos",
     },
     {
       description: "“Sinking”, 2020. Acrylic on paper",
-      image:
-        "/images/painting7.jpg",
+      image: "/images/painting7.jpg",
       category: "Paintings",
     },
     {
       description: "“Wake up call”, 2020. Acrylic on paper",
-      image:
-        "/images/painting5.jpg",
+      image: "/images/painting5.jpg",
       category: "Paintings",
     },
     {
       description: "Portugal, 2020",
-      image:
-        "/images/tattoo3.jpg",
+      image: "/images/tattoo3.jpg",
       category: "Tattoos",
     },
     {
       description: "“The rebirthing”, 2020. Digital comic",
-      image:
-        "/images/digital3.jpg",
+      image: "/images/digital3.jpg",
       category: "Digital",
     },
     {
       description: "Portugal, 2020",
-      image:
-        "/images/tattoo8.jpg",
+      image: "/images/tattoo8.jpg",
       category: "Tattoos",
     },
   ],
