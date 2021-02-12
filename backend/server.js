720fdc7863735ccb13e47591ea33c03bf7642a26
Joao Lopes <joaorfclopes@gmail.com>
2021-02-12 11:05:57 +0000

    Redirect to https added

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    else next();
+  });
+}
+
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/sweevil", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
