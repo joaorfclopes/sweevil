cb960fae89e76e912e257efc6247ba725c92aaae
Joao Lopes <joaorfclopes@gmail.com>
2021-02-21 17:55:19 +0000

    Lettering added to home screen mobile

--- frontend/src/components/Illustration.jsx
+++ frontend/src/components/Illustration.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { ReactComponent as Lettering } from "../assets/svg/home/lettering.svg";
 
 export default function Illustration(props) {
   return (
     <div className="illustration">
       <div className="illustration-lg">{props.illustrationLg}</div>
-      <div className="illustration-sm">{props.illustrationSm}</div>
+      <div className="illustration-sm">
+        {props.illustrationSm}
+        <Lettering />
+      </div>
     </div>
   );
 }
