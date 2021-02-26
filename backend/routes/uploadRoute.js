6ab66d5f706bd916a568fafec64a8d0f6777c8ea
Joao Lopes <joaorfclopes@gmail.com>
2021-02-26 21:23:10 +0000

    Upload to S3 changed

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -13,7 +13,7 @@ aws.config.update({
 const s3 = new aws.S3();
 const storageS3 = multerS3({
   s3,
-  bucket: "sweevil-bucket",
+  bucket: "sweevil",
   acl: "public-read",
   contentType: multerS3.AUTO_CONTENT_TYPE,
   key(req, file, cb) {
