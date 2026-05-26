d3e13b8e48f8a7bfe32e2a11929b62b76c210e65
João Lopes <joaorfclopes@gmail.com>
2026-05-26 18:13:35 +0100

    chore(test): add jest+supertest devDeps and jest config

---
+++ jest.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  testEnvironment: 'node',
+  testMatch: ['**/backend/__tests__/**/*.test.js'],
+  transform: {},
+  extensionsToTreatAsEsm: ['.js'],
+  globalSetup: './backend/__tests__/globalSetup.js',
+  globalTeardown: './backend/__tests__/globalTeardown.js',
+  setupFiles: ['./backend/__tests__/jestSetup.js'],
+  testTimeout: 30000,
+};
